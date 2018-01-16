d1e5e68bcd0972a72e4337e999bd5be3213d5ac2
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-16 13:54:12 -0800

    working locally

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userAgent = require('express-useragent');
+
+const port = process.env.PORT || 3000;
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(userAgent.express());
+
+const api = '/api/whoami';
+app.get(api, (req, res, next) => {
+	const language = req.acceptsLanguages();
+	const software = `OS: ${req.useragent.os}, Browser: ${req.useragent.browser}`;
+	const ipaddress = req.ip;
+
+	res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(3000, () => {
-  console.log('Server listening on port 3000!');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
 });
