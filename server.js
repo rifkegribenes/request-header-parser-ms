51ad7489f797eb1f7683169e18b5abeaeeeb39db
Glitch (rifkegribenes-request-header-parser-ms) <none>
2018-01-17 00:49:50 +0000

    parse headers at home route too

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(userAgent.express());
 
+// return the user agent, ip address, and language at the api/whoami route
 const api = '/api/whoami';
 app.get(api, (req, res, next) => {
 	const language = req.acceptsLanguages();
@@ -19,8 +20,13 @@ app.get(api, (req, res, next) => {
 	res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
 })
 
+// return the same info at the home route also because i don't feel like building a UI for this thing
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const language = req.acceptsLanguages();
+	const software = `OS: ${req.useragent.os}, Browser: ${req.useragent.browser}`;
+	const ipaddress = req.ip;
+
+	res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
 });
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
