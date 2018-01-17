2ee0e8c9f70ae4fa5d8c0584c692bf17c559fd0c
Glitch (strong-zone) <none>
2018-01-17 00:29:57 +0000

    Import of rifkegribenes/request-header-parser-ms

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userAgent = require('express-useragent');
 
-// init project
-var express = require('express');
-var app = express();
+const port = process.env.PORT || 3000;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(userAgent.express());
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const api = '/api/whoami';
+app.get(api, (req, res, next) => {
+	const language = req.acceptsLanguages();
+	const software = `OS: ${req.useragent.os}, Browser: ${req.useragent.browser}`;
+	const ipaddress = req.ip;
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+	res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
 });
