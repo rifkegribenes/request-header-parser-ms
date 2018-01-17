a47586bd9176dfee51257c20377553e200bf384d
Glitch (rifkegribenes-request-header-parser-ms) <none>
2018-01-17 03:55:23 +0000

    🚘👰 Checkpoint server.js:417995/9

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get(api, (req, res, next) => {
 	const ipaddress = req.ip;
 
 	res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
-})
+});
 
 // return the same info at the home route also because i don't feel like building a UI for this thing
 app.get('/', (req, res) => {
@@ -30,4 +30,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
-})
+});
+  
